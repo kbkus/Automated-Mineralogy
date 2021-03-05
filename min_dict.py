f4e891d48c9db1ff68d0c4c2fcd753f57653eff4
kbkus <kacikus@gmail.com>
2021-03-05 11:46:32 -0700

    create mineral dictionary file

---
+++ min_dict.py
@@ -0,0 +1,13 @@
+# dictionary of minerals and their RGB values
+MINERALS = {
+    
+    'quartz':([85,0,255],1),
+    'plagioclase':([71,213,213],2),
+    'orthoclase':([255,32,103],3),
+    'muscovite':([251,119,255],4),
+    'biotite':([255,170,0],5),
+    'clay minerals':([255,85,0],6),
+    'calcite':([],7),
+    'dolomite':([],8),
+    
+}
