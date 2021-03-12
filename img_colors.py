caae51946faba60c39950e4df423b9c48a0b0584
kbkus <kacikus@gmail.com>
2021-03-12 08:54:52 -0700

    updates to model building

--- img_colors.py
+++
@@ -1,8 +0,0 @@
-def show_color_counts(img):
-    ''' Input an image in the format of a np.array and return
-    subplots of each pixel color plotted with the RGB color value and number
-    of pixels in the image that are that color.'''
-    img = img.reshape((-1, 3))
-    values, counts = np.unique(img, axis=0, return_counts=True)
-    
-plt.imshow([[[0, 155, 85]]])
