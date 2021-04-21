9efb7ee359125370e233f1b2c1f1a9e5354ef144
kbkus <kacikus@gmail.com>
2021-04-21 10:55:06 -0600

    updated models.py

--- models.py
+++ models.py
@@ -27,28 +27,38 @@ def model_orig(pretrained=False):
     conv2 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', padding='same')(conv2)
     pool2 = tf.keras.layers.MaxPooling2D((2,2))(conv2)
 
-    # decoder layers
     conv3 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', padding='same')(pool2)
     conv3 = tf.keras.layers.Dropout(0.2)(conv3)
     conv3 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', padding='same')(conv3)
+    pool3 = tf.keras.layers.MaxPooling2D((2,2))(conv3)
+
+    # decoder layers
+    conv4 = tf.keras.layers.Conv2D(256, (3,3), activation='relu', padding='same')(pool3)
+    conv4 = tf.keras.layers.Dropout(0.2)(conv4)
+    conv4 = tf.keras.layers.Conv2D(256, (3,3), activation='relu', padding='same')(conv4)
 
     # concat intermediate encoder outputs with intermediate decoder outputs, which is the skip connection
-    up1 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling2D((2,2))(conv3), conv2], axis=-1)
-    conv4 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', padding='same')(up1)
-    conv4 = tf.keras.layers.Dropout(0.2)(conv4)
-    conv4 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', padding='same')(conv4)
-
-    up2 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling2D((2,2))(conv4), conv1], axis=-1)
-    conv5 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', padding='same')(up2)
+    up1 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling2D((2,2))(conv4), conv3], axis=-1)
+    conv5 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', padding='same')(up1)
     conv5 = tf.keras.layers.Dropout(0.2)(conv5)
-    conv5 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', padding='same')(conv5)
+    conv5 = tf.keras.layers.Conv2D(128, (3,3), activation='relu', padding='same')(conv5)
+
+    up2 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling2D((2,2))(conv5), conv2], axis=-1)
+    conv6 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', padding='same')(up2)
+    conv6 = tf.keras.layers.Dropout(0.2)(conv6)
+    conv6 = tf.keras.layers.Conv2D(64, (3,3), activation='relu', padding='same')(conv6)
+
+    up3 = tf.keras.layers.concatenate([tf.keras.layers.UpSampling2D((2,2))(conv6), conv1], axis=-1)
+    conv7 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', padding='same')(up3)
+    conv7 = tf.keras.layers.Dropout(0.2)(conv7)
+    conv7 = tf.keras.layers.Conv2D(32, (3,3), activation='relu', padding='same')(conv7)
 
     # get output with proper number of classes
-    out = tf.keras.layers.Conv2D(n_classes, (1,1), padding='same')(conv5)
+    out = tf.keras.layers.Conv2D(n_classes, (1,1), padding='same')(conv7)
 
     model = get_segmentation_model(img_input, out)
     model.train(
-        train_images = '../dataset/ppl_train_images/',
+        train_images = '../dataset/xpl_train_images/',
         train_annotations = '../dataset/train_segmentation/',
         epochs = n_epochs)
     
